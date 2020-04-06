6f3970ccb88fe56c5dd6b6a59000e153d779cc58
David Parks <davidthomasparks@gmail.com>
2020-04-06 10:50:40 -0500

    add dollar to price on product card

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -4,8 +4,8 @@
             <img class="object-cover h-64 w-full overflow-hidden" :src="product.images[0].src" :alt="product.images[0].altText">
         </nuxt-link>
         <div class="p-4 text-center">
-            <span class="flex items-center absolute top-0 left-0 rounded-full py-2 px-4 bg-gray-900 text-white m-2">
-                <p class="">{{product.variants[0].price}}</p>
+            <span class="flex items-center absolute top-0 left-0 rounded-full py-2 px-4 bg-gray-900 text-white m-2 shadow">
+                <p class="">${{product.variants[0].price}}</p>
             </span>
             <h3 class="">{{product.title}}</h3>
         </div>
