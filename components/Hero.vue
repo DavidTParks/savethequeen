f0f192da4361f5d28893f5e1b3b3eccafa77e803
David Parks <davidthomasparks@gmail.com>
2020-04-05 20:36:35 -0500

    remove login

--- components/Hero.vue
+++ components/Hero.vue
@@ -45,11 +45,6 @@
               <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out">Product</a>
               <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out">Cart</a>
             </div>
-            <div>
-              <a href="#" class="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100 hover:text-indigo-700 focus:outline-none focus:bg-gray-100 focus:text-indigo-700 transition duration-150 ease-in-out">
-                Log in
-              </a>
-            </div>
           </div>
         </div>
       </div>
