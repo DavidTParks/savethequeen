34077bd1948e866cf0c87490ef0097ea19b34056
David Parks <davidthomasparks@gmail.com>
2020-04-08 11:48:25 -0500

    remove webp format from imgix url for mobile

--- components/Hero.vue
+++ components/Hero.vue
@@ -96,7 +96,7 @@
     </div>
   </div>
   <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-    <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="http://savethequeencandleco.imgix.net/%C2%A9+Gloss+Photography+Studios-85.jpg?q=1&fm=webp" alt="" />
+    <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://savethequeencandleco.imgix.net/%C2%A9+Gloss+Photography+Studios-85.jpg?q=1" alt="" />
   </div>
 </div>
 </template>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,85 +1,78 @@
-
 export default {
-  mode: 'universal',
-  /*
+	mode: 'universal',
+	/*
   ** Headers of the page
   */
-  head: {
-    title: 'Save The Queen Candle Co.',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
+	head: {
+		title: 'Save The Queen Candle Co.',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+		],
+		link: [ { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' } ]
+	},
+	/*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
-  /*
+	loading: { color: '#fff' },
+	/*
   ** Global CSS
   */
-  css: [
-  ],
-  /*
+	css: [],
+	/*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-  ],
-  /*
+	plugins: [],
+	/*
   ** Nuxt.js dev-modules
   */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss',
-  ],
-  /*
+	buildModules: [
+		// Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+		'@nuxtjs/tailwindcss'
+	],
+	/*
   ** Nuxt.js modules
   */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-    'nuxt-shopify',
-    // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv',
-  ],
+	modules: [
+		// Doc: https://axios.nuxtjs.org/usage
+		'@nuxtjs/axios',
+		'@nuxtjs/pwa',
+		'nuxt-shopify',
+		// Doc: https://github.com/nuxt-community/dotenv-module
+		'@nuxtjs/dotenv'
+	],
 
-  shopify: {
-    /**
+	shopify: {
+		/**
      * Your shopify domain
      */
-    domain: 'savethequeencandles.myshopify.com',
+		domain: 'savethequeencandles.myshopify.com',
 
-    /**
+		/**
      * Your shopify storefront access token
      */
-    storefrontAccessToken: 'd09939972403684d89b1d580719c9ae4',
+		storefrontAccessToken: 'd09939972403684d89b1d580719c9ae4',
 
-    /**
+		/**
      * This will be larger than the optimized version, as it will contain all fields that are available in the
      * Storefront API. (https://help.shopify.com/en/api/custom-storefronts/storefront-api/reference)
      * This should only be used when you need to add custom queries to supplement the JS Buy SDK queries.
      */
-    unoptimized: false,
-  },
-  /*
+		unoptimized: false
+	},
+	/*
   ** Axios module configuration
   ** See https://axios.nuxtjs.org/options
   */
-  axios: {
-  },
-  /*
+	axios: {},
+	/*
   ** Build configuration
   */
-  build: {
-    /*
+	build: {
+		/*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
-    }
-  }
-}
+		extend(config, ctx) {}
+	}
+};
