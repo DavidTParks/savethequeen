a0cda90ce416f6fa10cab4485ff8a6e99923283b
David Parks <davidthomasparks@gmail.com>
2020-04-10 00:08:14 -0500

    latest version of nuxt

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,39 +27,10 @@ html {
   box-sizing: border-box;
 }
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
+.page-enter-active, .page-leave-active {
+  transition: opacity .3s;
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.page-enter, .page-leave-to {
+  opacity: 0;
 }
 </style>
--- pages/candles.vue
+++ pages/candles.vue
@@ -64,3 +64,12 @@ export default {
   },
 }
 </script>
+
+<style>
+.page-enter-active, .page-leave-active {
+  transition: opacity .3s;
+}
+.page-enter, .page-leave-to {
+  opacity: 0;
+}
+</style>
