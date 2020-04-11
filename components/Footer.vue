89eb3fcbf2e36452219f94911ed91feb55c37272
David Parks <davidthomasparks@gmail.com>
2020-04-11 09:32:23 -0500

    featured section

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-24 bg-gray-100 py-12 flex items-center justify-center">
+  <div class="h-24 bg-gray-100 mt-12 flex items-center justify-center">
     <p class="text-center text-xs md:text-base text-gray-500">© 2020 Save The Queen Candle Company. All rights reserved.</p>
   </div>
 </template>
