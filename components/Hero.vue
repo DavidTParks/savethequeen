cb4e5f71f76218f531dc10b714f48f8e91b356df
David Parks <davidthomasparks@gmail.com>
2020-04-12 17:12:03 -0500

    custom pours

--- components/Hero.vue
+++ components/Hero.vue
@@ -6,9 +6,9 @@
         <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start">
           <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
             <div class="flex items-center justify-between w-full md:w-auto">
-              <a href="#">
+              <nuxt-link to="/" href="#">
                 <img class="h-8 w-auto sm:h-10" src="~/assets/images/logo.svg" alt="" />
-              </a>
+              </nuxt-link>
               <div class="flex items-center md:hidden mr-2">
                 <button @click="open = true" type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                   <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -26,7 +26,7 @@
           </div>
           <div class="hidden md:block md:ml-10 md:pr-4">
             <nuxt-link to="/candles" class="font-medium text-gray-900 hover:text-gray-800 focus:outline-none focus:text-gray-900 transition duration-150 ease-in-out">Candles</nuxt-link>
-            <a href="#" class="ml-8 font-medium text-gray-900 hover:text-gray-800 focus:outline-none focus:text-gray-900 transition duration-150 ease-in-out">Custom Pours</a>
+            <nuxt-link to="/custom-pours" class="ml-8 font-medium text-gray-900 hover:text-gray-800 focus:outline-none focus:text-gray-900 transition duration-150 ease-in-out">Custom Pours</nuxt-link>
             <nuxt-link class="ml-8 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition duration-150 ease-in-out relative" to="/cart">
                 <svg class="inline-block" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z" stroke="#4A5568" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
