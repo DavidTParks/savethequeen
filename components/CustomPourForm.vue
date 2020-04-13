0b9b3006bffdf0a7b9a635e58fd1d0e79d7e2e3e
David Parks <davidthomasparks@gmail.com>
2020-04-13 11:00:10 -0500

    Add thank you page

--- components/CustomPourForm.vue
+++ components/CustomPourForm.vue
@@ -1,5 +1,5 @@
 <template>
-<form class="w-full max-w-lg mx-auto" method="post" data-netlify="true" name="custom-pour">
+<form class="w-full max-w-lg mx-auto" method="post" data-netlify="true" name="custom-pour" action="/thank-you">
   <div class="flex flex-wrap -mx-3 mb-6">
     <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
---
+++ pages/thank-you.vue
@@ -0,0 +1,11 @@
+<template>
+<div>
+<h1>Thank you!</h1>
+</div>
+</template>
+
+<script>
+export default {
+    layout: 'detail',
+}
+</script>
