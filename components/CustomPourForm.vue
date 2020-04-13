e518ee693973f8ac71441b1c9b1fad11bb37fc70
David Parks <davidthomasparks@gmail.com>
2020-04-13 11:06:18 -0500

    add honeypot field and hidden field with form name

--- components/CustomPourForm.vue
+++ components/CustomPourForm.vue
@@ -1,10 +1,11 @@
 <template>
-<form class="w-full max-w-lg mx-auto" method="post" data-netlify="true" name="custom-pour" action="/thank-you">
+<form class="w-full max-w-lg mx-auto" method="post" data-netlify="true" data-netlify-honeypot="bot-field" name="custom-pour" action="/thank-you">
   <div class="flex flex-wrap -mx-3 mb-6">
     <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
         First Name *
       </label>
+      <input type="hidden" name="form-name" value="custom-pour" />
       <input name="first-name" required class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="Jane">
     </div>
     <div class="w-full md:w-1/2 px-3">
