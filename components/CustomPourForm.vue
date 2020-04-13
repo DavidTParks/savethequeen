ba1288fa09f22a3c1c4e40c000233fe8931e4af7
David Parks <davidthomasparks@gmail.com>
2020-04-13 10:46:48 -0500

    custom pour initial and netlify form

---
+++ components/CustomPourForm.vue
@@ -0,0 +1,70 @@
+<template>
+<form class="w-full max-w-lg mx-auto" netlify>
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+        First Name *
+      </label>
+      <input required class="appearance-none block w-full bg-gray-200 text-gray-700  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane">
+    </div>
+    <div class="w-full md:w-1/2 px-3">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+        Last Name *
+      </label>
+      <input required class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe">
+    </div>
+  </div>
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full px-3">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+        Email *
+      </label>
+      <input required class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-email" type="email" placeholder="@">
+      <p class="text-gray-600 text-xs italic">We will reach out to this email.</p>
+    </div>
+  </div>
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full px-3">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+        Phone Number *
+      </label>
+      <input required class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="tel" placeholder="444-444-4444">
+    </div>
+  </div>
+  <div class="flex flex-wrap -mx-3 mb-2">
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-scents">
+        Scent *
+      </label>
+      <div class="relative">
+        <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-scent">
+          <option>Natural Honey</option>
+          <option>Lavender</option>
+          <option>Lemongrass</option>
+          <option>Lemon Rosemary</option>
+        </select>
+        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+        </div>
+      </div>
+    </div>
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
+        Item To Pour *
+      </label>
+      <input required class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="">
+    </div>
+  </div>
+  <div class="flex flex-wrap -mx-3 mt-12">
+    <div class="w-full px-3">
+        <button class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-red-600 hover:bg-red-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10" type="submit">Send</button>
+    </div>
+  </div>
+</form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- pages/custom-pours.vue
+++ pages/custom-pours.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="min-h-screen">
-      <div class="max-w-screen-xl mx-auto pr-4 sm:pr-6 lg:pr-8">
-          <h3 class="text-center mt-12">Custom Pour Placeholder</h3>
+      <div class="max-w-screen-xl mx-auto mx-auto px-4 sm:px-6 lg:px-8 mt-12">
+        <div class="flex flex-col sm:flex-row">
+          <div class="w-full sm:w-1/2">
+            <h1 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 md:mt-5 md:text-xl mx-auto sm:text-5xl sm:leading-none md:text-6xl text-left mt-12">Custom Pours</h1>
+            <p class="mt-3 text-base text-gray-700 sm:mt-5 sm:text-lg md:mt-5 md:text-xl mx-auto">We can custom pour a unique candle in the glass container of your choice for $3.00 per ounce. Many of our customers have turned inherited family glassware into functional and meaningful treasures that can be repoured over & over again.</p>
+            <p class="mt-3 text-base text-gray-700 sm:mt-5 sm:text-lg md:mt-5 md:text-xl mx-auto">To inquire about a custom pour, please fill out the contact form below.</p>
+          </div>
+          <div class="w-full sm:w-1/2">
+            <img class="mt-5 sm:text-lg sm:max-w-xl md:mt-5 md:text-xl mx-auto" src="http://savethequeencandleco.imgix.net/custom-pour-86.jpg?q=1"/>
+          </div>
+        </div>
+        <div class="mx-auto">
+          <CustomPourForm class="mt-12"/>
+        </div>
       </div>
   </div>
 </template>
 
 <script>
+import CustomPourForm from '~/components/CustomPourForm.vue'
 export default {
    layout: "detail",
+   components: {
+     CustomPourForm
+   }
 }
 </script>
