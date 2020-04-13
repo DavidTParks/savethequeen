cc9293d49fca126980d9922f1d5260071d082ac1
David Parks <davidthomasparks@gmail.com>
2020-04-13 10:52:49 -0500

    Add additional netlify fields to form

--- components/CustomPourForm.vue
+++ components/CustomPourForm.vue
@@ -1,11 +1,12 @@
 <template>
-<form class="w-full max-w-lg mx-auto" netlify>
+<form class="w-full max-w-lg mx-auto" method="post" data-netlify-honeypot="bot-field" data-netlify="true" name="custom-pour">
   <div class="flex flex-wrap -mx-3 mb-6">
     <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
         First Name *
       </label>
       <input required class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="Jane">
+      <input type="hidden" name="form-name" value="ask-question" />
     </div>
     <div class="w-full md:w-1/2 px-3">
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
