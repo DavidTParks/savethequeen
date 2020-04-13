98926dd28f2008854bfd5a4b4cf3fce7330d9348
David Parks <davidthomasparks@gmail.com>
2020-04-13 11:23:44 -0500

    margin on input

--- components/CustomPourForm.vue
+++ components/CustomPourForm.vue
@@ -37,7 +37,7 @@
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-scents">
         Optional Scent
       </label>
-      <div class="relative">
+      <div class="relative mb-3">
         <select name="scent" class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-scent">
           <option>Fragrance Free</option>
           <option>Natural Honey</option>
