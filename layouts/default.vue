fd2adc8188826c0578633143516e69787c328cbb
David Parks <davidthomasparks@gmail.com>
2020-04-13 22:19:33 -0500

    Refactor out css to seperate file for global insertion

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,23 +13,3 @@ export default {
   }
 }
 </script>
-
-<style>
-html {
-  font-family: 'Source Sans Pro', sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-.page-enter-active, .page-leave-active {
-  transition: opacity .3s;
-}
-.page-enter, .page-leave-to {
-  opacity: 0;
-}
-</style>
