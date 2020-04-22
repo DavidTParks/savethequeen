05efb98d469f69e79ef5e296f064f968c27b2226
David Parks <davidthomasparks@gmail.com>
2020-04-22 18:19:44 -0500

    add sass to candles page file

--- pages/candles.vue
+++ pages/candles.vue
@@ -140,7 +140,6 @@ export default {
 .page-leave-to {
   opacity: 0;
 }
-
 /* purgecss start ignore */
 // Variables
 $vue-content-placeholders-primary-color: #ccc !default;
@@ -151,125 +150,184 @@ $vue-content-placeholders-spacing: 10px !default;
 
 // Animations
 @keyframes vueContentPlaceholdersAnimation {
-  0% {
-    transform: translate3d(-30%, 0, 0);
-  }
+	0% {
+		transform: translate3d(-30%, 0, 0);
+	}
 
-  100% {
-    transform: translate3d(100%, 0, 0);
-  }
+	100% {
+		transform: translate3d(100%, 0, 0);
+	}
 }
 
 // Mixins
 @mixin vue-content-placeholders {
-  position: relative;
-  overflow: hidden;
-  height: $vue-content-placeholders-line-height;
-  background: $vue-content-placeholders-secondary-color;
+	position: relative;
+	overflow: hidden;
+	height: $vue-content-placeholders-line-height;
+	background: $vue-content-placeholders-secondary-color;
 
-  .vue-content-placeholders-is-rounded & {
-    border-radius: $vue-content-placeholders-border-radius;
-  }
+	.vue-content-placeholders-is-rounded & {
+		border-radius: $vue-content-placeholders-border-radius;
+	}
 
-  .vue-content-placeholders-is-centered & {
-    margin-left: auto;
-    margin-right: auto;
-  }
+	.vue-content-placeholders-is-centered & {
+		margin-left: auto;
+		margin-right: auto;
+	}
 
-  .vue-content-placeholders-is-animated &::before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    max-width: 1000px;
-    height: 100%;
-    background: linear-gradient(
-      to right,
-      transparent 0%,
-      darken($vue-content-placeholders-secondary-color, 5%) 15%,
-      transparent 30%
-    );
-    animation-duration: 1.5s;
-    animation-fill-mode: forwards;
-    animation-iteration-count: infinite;
-    animation-name: vueContentPlaceholdersAnimation;
-    animation-timing-function: linear;
-  }
+	.vue-content-placeholders-is-animated &::before {
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		max-width: 1000px;
+		height: 100%;
+		background: linear-gradient(
+			to right,
+			transparent 0%,
+			darken($vue-content-placeholders-secondary-color, 5%) 15%,
+			transparent 30%
+		);
+		animation-duration: 1.5s;
+		animation-fill-mode: forwards;
+		animation-iteration-count: infinite;
+		animation-name: vueContentPlaceholdersAnimation;
+		animation-timing-function: linear;
+	}
 }
 
 @mixin vue-content-placeholders-spacing {
-  [class^="vue-content-placeholders-"] + & {
-    margin-top: 2 * $vue-content-placeholders-spacing;
-  }
+	[class^="vue-content-placeholders-"] + & {
+		margin-top: 2 * $vue-content-placeholders-spacing;
+	}
 }
 
 // Styles
 .vue-content-placeholders-heading {
-  @include vue-content-placeholders-spacing;
-  display: flex;
+	@include vue-content-placeholders-spacing;
+	display: flex;
 
-  &__img {
-    @include vue-content-placeholders;
-    width: 2 * $vue-content-placeholders-line-height + 3 *
-      $vue-content-placeholders-spacing;
-    height: 2 * $vue-content-placeholders-line-height + 3 *
-      $vue-content-placeholders-spacing;
-    margin-right: 1.5 * $vue-content-placeholders-spacing;
-  }
+	&__img {
+		@include vue-content-placeholders;
+		width: 2 * $vue-content-placeholders-line-height + 3 * $vue-content-placeholders-spacing;
+		height: 2 * $vue-content-placeholders-line-height + 3 * $vue-content-placeholders-spacing;
+		margin-right: 1.5 * $vue-content-placeholders-spacing;
+	}
 
-  &__content {
-    display: flex;
-    flex: 1;
-    flex-direction: column;
-    justify-content: center;
-  }
+	&__content {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		justify-content: center;
+	}
 
-  &__title {
-    @include vue-content-placeholders;
-    width: 85%;
-    margin-bottom: $vue-content-placeholders-spacing;
-    background: $vue-content-placeholders-primary-color;
-  }
+	&__title {
+		@include vue-content-placeholders;
+		width: 85%;
+		margin-bottom: $vue-content-placeholders-spacing;
+		background: $vue-content-placeholders-primary-color;
+	}
 
-  &__subtitle {
-    @include vue-content-placeholders;
-    width: 90%;
-  }
+	&__subtitle {
+		@include vue-content-placeholders;
+		width: 90%;
+	}
+}
+
+.vue-content-placeholders-heading__title {
+	position: relative;
+	overflow: hidden;
+	height: 15px;
+	background: #eee;
+	width: 85%;
+	margin-bottom: 10px;
+	background: #ccc;
+}
+
+.vue-content-placeholders-is-animated .vue-content-placeholders-heading__title::before {
+	content: "";
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100vw;
+	max-width: 1000px;
+	height: 100%;
+	background: linear-gradient(to right, transparent 0%, #e1e1e1 15%, transparent 30%);
+	-webkit-animation-duration: 1.5s;
+	animation-duration: 1.5s;
+	-webkit-animation-fill-mode: forwards;
+	animation-fill-mode: forwards;
+	-webkit-animation-iteration-count: infinite;
+	animation-iteration-count: infinite;
+	-webkit-animation-name: vueContentPlaceholdersAnimation;
+	animation-name: vueContentPlaceholdersAnimation;
+	-webkit-animation-timing-function: linear;
+	animation-timing-function: linear;
+}
+
+.vue-content-placeholders-heading__subtitle {
+	position: relative;
+	overflow: hidden;
+	height: 15px;
+	background: #eee;
+	width: 90%;
+}
+
+.vue-content-placeholders-is-animated .vue-content-placeholders-heading__subtitle::before {
+	content: "";
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100vw;
+	max-width: 1000px;
+	height: 100%;
+	background: linear-gradient(to right, transparent 0%, #e1e1e1 15%, transparent 30%);
+	-webkit-animation-duration: 1.5s;
+	animation-duration: 1.5s;
+	-webkit-animation-fill-mode: forwards;
+	animation-fill-mode: forwards;
+	-webkit-animation-iteration-count: infinite;
+	animation-iteration-count: infinite;
+	-webkit-animation-name: vueContentPlaceholdersAnimation;
+	animation-name: vueContentPlaceholdersAnimation;
+	-webkit-animation-timing-function: linear;
+	animation-timing-function: linear;
 }
 
 .vue-content-placeholders-text {
-  @include vue-content-placeholders-spacing;
+	@include vue-content-placeholders-spacing;
 
-  &__line {
-    @include vue-content-placeholders;
-    width: 100%;
-    margin-bottom: $vue-content-placeholders-spacing;
+	&__line {
+		@include vue-content-placeholders;
+		width: 100%;
+		margin-bottom: $vue-content-placeholders-spacing;
 
-    &:nth-child(4n + 1) {
-      width: 80%;
-    }
+		&:nth-child(4n + 1) {
+			width: 80%;
+		}
 
-    &:nth-child(4n + 2) {
-      width: 100%;
-    }
+		&:nth-child(4n + 2) {
+			width: 100%;
+		}
 
-    &:nth-child(4n + 3) {
-      width: 70%;
-    }
+		&:nth-child(4n + 3) {
+			width: 70%;
+		}
 
-    &:nth-child(4n + 4) {
-      width: 85%;
-    }
-  }
+		&:nth-child(4n + 4) {
+			width: 85%;
+		}
+	}
 }
 
 .vue-content-placeholders-img {
-  @include vue-content-placeholders;
-  @include vue-content-placeholders-spacing;
-  width: 100%;
-  height: 120px;
+	@include vue-content-placeholders;
+	@include vue-content-placeholders-spacing;
+	width: 100%;
+	height: 120px;
 }
 
 /* purgecss end ignore */
+
 </style>
