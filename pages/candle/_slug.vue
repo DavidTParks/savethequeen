d1773c6049405c12f54c35bc4914ccec91b13589
David Parks <davidthomasparks@gmail.com>
2020-04-22 00:02:45 -0500

    related products two rows

--- pages/candle/_slug.vue
+++ pages/candle/_slug.vue
@@ -59,7 +59,7 @@
             <div class="py-12">
                 <h2 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 mt-12 lg:text-center"
                 >Related Products</h2>
-                <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mt-12">
+                <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-4 mt-12">
                     <template v-for="index in 4">
                         <ProductCard :key="products[index].id" :product="products[index]"></ProductCard>
                     </template>
