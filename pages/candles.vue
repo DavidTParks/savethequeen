09085394ab1f42120a69dfd38ce2fa6676f51afc
David Parks <davidthomasparks@gmail.com>
2020-04-22 18:12:45 -0500

    add placeholder sass to placeholder file

--- pages/candles.vue
+++ pages/candles.vue
@@ -1,58 +1,83 @@
 <template>
-<div class="min-h-screen bg-gray-100 pb-24">
-  <div class="py-8">
+  <div class="min-h-screen bg-gray-100 pb-24">
+    <div class="py-8">
       <div class="max-w-screen-xl mx-auto sm:pr-6 lg:pr-6">
-            <ul class="flex items-center px-6 sm:w-1/4">
-              <li class="breadcrumb-style">
-                  <nuxt-link class="mr-2" to="/">Home</nuxt-link>
-                  <svg class="h-4 w-4" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M9 5L16 12L9 19" stroke="#4A5568" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                  </svg>
-              </li>
-              <li class="breadcrumb-style">
-                  <nuxt-link class="mr-2" to="/candles">Candles</nuxt-link>
-              </li>
-          </ul>
+        <ul class="flex items-center px-6 sm:w-1/4">
+          <li class="breadcrumb-style">
+            <nuxt-link class="mr-2" to="/">Home</nuxt-link>
+            <svg
+              class="h-4 w-4"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9 5L16 12L9 19"
+                stroke="#4A5568"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </li>
+          <li class="breadcrumb-style">
+            <nuxt-link class="mr-2" to="/candles">Candles</nuxt-link>
+          </li>
+        </ul>
         <div class="flex flex-col sm:flex-row justify-between">
-          <div class="sm:w-1/4">
-          </div>
+          <div class="sm:w-1/4"></div>
           <div class="relative sm:w-3/4 mt-6 sm:mt-0 sm:ml-6 mx-4 sm:mr-2">
-            <input v-model="search" placeholder="Search for candles..." class="ml-0 pl-12 sm:ml-2 appearance-none w-full block bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"/>
-            <svg class="absolute left-0 top-0 bottom-0 right-0 my-auto sm:ml-6 ml-4 h-5 w-5" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="#4A5568" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <input
+              v-model="search"
+              placeholder="Search for candles..."
+              class="ml-0 pl-12 sm:ml-2 appearance-none w-full block bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            />
+            <svg
+              class="absolute left-0 top-0 bottom-0 right-0 my-auto sm:ml-6 ml-4 h-5 w-5"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+                stroke="#4A5568"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
             </svg>
           </div>
         </div>
         <div class="flex-col flex sm:flex-row">
           <div class="w-full sm:w-1/4">
-            <FilterNav 
-            @collectionSelected="handleCollectionSorting" 
-            @priceSorted="handlePriceSorting"
-            @resetFilters="resetFilters"
-            :selected-price="price" 
-            :selected="selected"
+            <FilterNav
+              @collectionSelected="handleCollectionSorting"
+              @priceSorted="handlePriceSorting"
+              @resetFilters="resetFilters"
+              :selected-price="price"
+              :selected="selected"
             />
           </div>
           <div class="w-full sm:w-3/4">
-            <ProductList
-            :search="search"
-            :selected-price="price" 
-            />
+            <ProductList :search="search" :selected-price="price" />
           </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
-import NavBar from '~/components/NavBar.vue'
-import Hero from '~/components/Hero.vue'
-import MarketingSection from '~/components/MarketingSection'
-import ProductCard from '~/components/ProductCard.vue'
-import ProductList from '~/components/ProductList.vue'
-import FilterNav from '~/components/FilterNav.vue'
+import NavBar from "~/components/NavBar.vue";
+import Hero from "~/components/Hero.vue";
+import MarketingSection from "~/components/MarketingSection";
+import ProductCard from "~/components/ProductCard.vue";
+import ProductList from "~/components/ProductList.vue";
+import FilterNav from "~/components/FilterNav.vue";
 export default {
   components: {
     NavBar,
@@ -64,50 +89,55 @@ export default {
   },
   data() {
     return {
-      price: '',
-      search: '',
-      selected: '',
+      price: "",
+      search: "",
+      selected: "",
       products: []
-    }
+    };
   },
   methods: {
     handleCollectionSorting(collection) {
-      if(this.selected === collection.id) {
-        this.selected = '';
+      if (this.selected === collection.id) {
+        this.selected = "";
         this.$router.push(this.$route.path);
       } else {
         this.selected = collection.id;
-        this.$router.push({path: this.$route.path, query: { collection: collection.id }});
+        this.$router.push({
+          path: this.$route.path,
+          query: { collection: collection.id }
+        });
       }
     },
     handlePriceSorting(price) {
-      if(this.price === price.value) {
-        this.price = '';
+      if (this.price === price.value) {
+        this.price = "";
       } else {
         this.price = price.value;
       }
     },
     resetFilters() {
       this.$router.push(this.$route.path);
-      this.price = '';
-      this.search = '';
-      this.selected = '';
+      this.price = "";
+      this.search = "";
+      this.selected = "";
     }
   },
-  layout: 'detail',
+  layout: "detail",
   computed: {
     checkout() {
       return this.$store.state.candles.checkout;
-    },
-  },
-}
+    }
+  }
+};
 </script>
 
 <style lang="scss">
-.page-enter-active, .page-leave-active {
-  transition: opacity .3s;
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.3s;
 }
-.page-enter, .page-leave-to {
+.page-enter,
+.page-leave-to {
   opacity: 0;
 }
 
@@ -118,8 +148,6 @@ $vue-content-placeholders-secondary-color: #eee !default;
 $vue-content-placeholders-border-radius: 6px !default;
 $vue-content-placeholders-line-height: 15px !default;
 $vue-content-placeholders-spacing: 10px !default;
-
-
 
 // Animations
 @keyframes vueContentPlaceholdersAnimation {
@@ -132,8 +160,6 @@ $vue-content-placeholders-spacing: 10px !default;
   }
 }
 
-
-
 // Mixins
 @mixin vue-content-placeholders {
   position: relative;
@@ -141,7 +167,7 @@ $vue-content-placeholders-spacing: 10px !default;
   height: $vue-content-placeholders-line-height;
   background: $vue-content-placeholders-secondary-color;
 
-  .vue-content-placeholders-is-rounded &  {
+  .vue-content-placeholders-is-rounded & {
     border-radius: $vue-content-placeholders-border-radius;
   }
 
@@ -151,14 +177,19 @@ $vue-content-placeholders-spacing: 10px !default;
   }
 
   .vue-content-placeholders-is-animated &::before {
-    content: '';
+    content: "";
     position: absolute;
     top: 0;
     left: 0;
     width: 100vw;
     max-width: 1000px;
     height: 100%;
-    background: linear-gradient(to right, transparent 0%, darken($vue-content-placeholders-secondary-color, 5%) 15%, transparent 30%);
+    background: linear-gradient(
+      to right,
+      transparent 0%,
+      darken($vue-content-placeholders-secondary-color, 5%) 15%,
+      transparent 30%
+    );
     animation-duration: 1.5s;
     animation-fill-mode: forwards;
     animation-iteration-count: infinite;
@@ -173,8 +204,6 @@ $vue-content-placeholders-spacing: 10px !default;
   }
 }
 
-
-
 // Styles
 .vue-content-placeholders-heading {
   @include vue-content-placeholders-spacing;
@@ -182,8 +211,10 @@ $vue-content-placeholders-spacing: 10px !default;
 
   &__img {
     @include vue-content-placeholders;
-    width: 2 * $vue-content-placeholders-line-height + 3 * $vue-content-placeholders-spacing;
-    height: 2 * $vue-content-placeholders-line-height + 3 * $vue-content-placeholders-spacing;
+    width: 2 * $vue-content-placeholders-line-height + 3 *
+      $vue-content-placeholders-spacing;
+    height: 2 * $vue-content-placeholders-line-height + 3 *
+      $vue-content-placeholders-spacing;
     margin-right: 1.5 * $vue-content-placeholders-spacing;
   }
 
