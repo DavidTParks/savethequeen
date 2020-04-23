3f78ee53de4c3370f2873e625e28ebf4aff3f6ef
David Parks <davidthomasparks@gmail.com>
2020-04-22 23:28:14 -0500

    product page add to cart buttons

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,7 @@ export default {
 		],
 		link: [ 
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;600;700;900&display=swap'}
     ]
 	},
 	/*
